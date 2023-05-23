49f6d9bf5eecde59ddf1f476f611d847b480b46d
Iryna Serhiienko <irishka.sergienko@gmail.com>
2023-05-24 01:52:20 +0300

    Added header, hero sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,11 @@
-const burger = document.querySelector(".js-burger-btn");
-const header = document.querySelector(".js-header");
+const burger = document.querySelector('.js-burger-btn');
+const header = document.querySelector('.js-header');
+const link = document.querySelectorAll('.js-nav-item');
 
-burger.addEventListener("click", () => {
-  header.classList.toggle("active");
+burger.addEventListener('click', () => {
+  header.classList.toggle('activeBurger');
+});
+
+link.addEventListener('click', () => {
+  link.classList.toggle('activeLink');
 });
